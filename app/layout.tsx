7579b3cdf0b33edbdece192ac51330b71f06d95d
manursutil <mrodsut@gmail.com>
2025-05-20 19:26:50 +0200

    Force rebuild to clear ESLint false error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/app/components/theme-provider";
 import NavBar from "./components/nav-bar";
